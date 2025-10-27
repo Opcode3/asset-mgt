d332ef48ab78c3fd0bb26411ad56f20d57ca33dd
Joseph Emmanuel Emeka <opcode3@gmail.com>
2025-10-27 07:04:44 +0100

    made changes to .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/authStore";
 
 // Create axios instance with base configuration
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "",
+  baseURL: import.meta.env.VITE_API_URL || "https://asset-mgt-api.onrender.com",
   timeout: 10000,
   // headers: {
   //   "Content-Type": "application/json",
